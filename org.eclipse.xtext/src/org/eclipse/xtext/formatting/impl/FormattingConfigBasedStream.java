b8c885cf509f80295fd803b5e85cd51f5bcc6d84
Tamas Miklossy <miklossy@itemis.de>
2020-04-13 09:39:45 +0200

    [#1443] Fix XtextFormatter behaviour on different line endings.
    - Modify the FormattingConfigBasedStream to handle the different line
    endings properly.
    - Implement corresponding XtextFormatterTest and
    GrammarAccessExtensionsTest test cases.
    
    Signed-off-by: Tamas Miklossy <miklossy@itemis.de>

--- org.eclipse.xtext/src/org/eclipse/xtext/formatting/impl/FormattingConfigBasedStream.java
+++ org.eclipse.xtext/src/org/eclipse/xtext/formatting/impl/FormattingConfigBasedStream.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 itemis AG (http://www.itemis.eu) and others.
+ * Copyright (c) 2009, 2020 itemis AG (http://www.itemis.eu) and others.
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
@@ -269,7 +269,9 @@ public class FormattingConfigBasedStream extends BaseTokenStream {
 			this.isHidden = isHidden;
 			this.leadingLocators = beforeLocators;
 			this.indent = indent;
-			this.leadingWS = leadingWS;
+			if (leadingWS != null) {
+				this.leadingWS = leadingWS.replaceAll("\r?\n", getLineSeparator());
+			}
 			this.hiddenTokenDefinition = hiddenTokenDefition;
 		}
 
